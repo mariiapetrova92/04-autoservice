ed851d74c4855ed932e9ecf4f16c615e41c14aae
mariiapetrova92 <32820604+mariiapetrova92@users.noreply.github.com>
2017-11-30 19:55:51 +0100

    finish

--- js/common.js
+++ js/common.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
+	
+	$(".mc_item_wrap ul").each(function() {
+		$(this).after("<div class='mc_item_wrap_af'></div>");
+	});
+
+	$(".mc_toggle").click(function() {
+	  if($(this).parent().parent().children("ul").is(":visible")){
+	  	$(this).parent().parent().children("ul").slideUp();
+		$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+	   }
+	   else{
+		$(".mc_item_wrap").removeClass("active");
+		$(this).parent().parent().addClass("active");
+		$(this).parent().parent().children("ul").slideDown();
+		$(this).parent().parent().children("mc_item_wrap_af").show();
+	   }
+
+	});
+
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
@@ -18,7 +37,7 @@ $(document).ready(function() {
 	//equalheight - одинаковая высота колонок
 	//Пример списка элементов:
 	//var eqElement = ".cat_container > div, .home_news > div"
-	var eqElement = ".element"
+	var eqElement = ".hi_item, .sb_content > div"
 	$(window).load(function(){equalheight(eqElement);}).resize(function(){equalheight(eqElement);});
 
 	//Masked Input Plugin
